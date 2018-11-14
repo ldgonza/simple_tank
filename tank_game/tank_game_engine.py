aff853b0bac60dbede7e6487f0d6d224d16a052f
Leonardo González <ldgonza@gmail.com>
2018-11-14 13:41:28 -0300

    Spawn set number of tanks in random locations

--- tank_game/tank_game_engine.py
+++ tank_game/tank_game_engine.py
@@ -2,37 +2,55 @@ from .engine.game_engine import GameEngine
 from tank_game_event_checker import TankGameEventChecker
 from tank import Tank
 from bullet import Bullet
+import random
+from tank_game_categories import PLAYER, TANK, BULLET
 
-from tank_game_categories import *
 
 class TankGameEngine(GameEngine):
     MAX_BULLETS = 3
+    MAX_TANKS = 5
 
     def __init__(self, screen):
         GameEngine.__init__(self, screen, TankGameEventChecker())
 
-        tank = Tank()
+        tank = self.add_tank((400, 300))
         tank.categories.append(PLAYER)
-        tank.place_at((400, 300))
-
-        self.entities.append(tank)
 
     def bullet_count(self):
+        return self.count(BULLET)
+
+    def tank_count(self):
+        return self.count(TANK)
+
+    def count(self, category):
         count = 0
         for e in self.entities.all():
-            if BULLET in e.categories: count += 1
+            if category in e.categories:
+                count += 1
 
-        return count;
+        return count
+
+    def add_tank(self, pos):
+        tank = Tank()
+        tank.place_at(pos)
+        tank.categories.append(TANK)
+        self.entities.append(tank)
+        return tank
+
+    def can_shoot(self):
+        not_at_bullet_cap = self.bullet_count() < TankGameEngine.MAX_BULLETS
+        want_to_shoot = self.event_checker.shooting
+        return want_to_shoot and not_at_bullet_cap
 
     def do_events(self, event_checker):
         GameEngine.do_events(self, event_checker)
-        
+
         if event_checker.direction is None:
             self.get_player().stop()
         else:
             self.get_player().turn_to(event_checker.direction)
 
-        if self.event_checker.shooting and self.bullet_count() < TankGameEngine.MAX_BULLETS:
+        if self.can_shoot():
             bullet = Bullet()
             bullet.categories.append(BULLET)
             bullet.place_at(self.get_player().rect.center)
@@ -40,9 +58,25 @@ class TankGameEngine(GameEngine):
 
             self.entities.append(bullet)
 
+        self.spawn_tank()
+
     def get_player(self):
         for e in self.entities.all():
-            if PLAYER in e.categories: return e
-        
+            if PLAYER in e.categories:
+                return e
 
-    
+    def get_random_pos(self):
+        x = random.uniform(0, self.screen_rect.width)
+        y = random.uniform(0, self.screen_rect.height)
+        return (x, y)
+
+    def get_tank(self, pos):
+        tank = Tank()
+        tank.categories.append(TANK)
+        tank.place_at(pos)
+
+    # spawn a tank somewhere
+    # if there's no more than a set number of tanks already there
+    def spawn_tank(self):
+        if self.tank_count() < TankGameEngine.MAX_TANKS:
+            self.add_tank(self.get_random_pos())
